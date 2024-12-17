abb27a6f509804004f1c9d0a0f7e97c8daa066a5
AzizBenGhorbel <mohamedaziz.benghorbel@insat.ucar.tn>
2024-12-18 00:10:27 +0100

    NEW: main navbar component

---
+++ src/app/shared/navbar/main/main-navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SmallNavbarComponent } from '../small/small-navbar.component';
+import { SettingsNavBarComponent } from '../settings/settings-navbar.component';
+import { ButtonSecondaryComponent } from '../../button-secondary/button-secondary.component';
+
+@Component({
+  selector: 'app-main-navbar',
+  standalone: true,
+  imports: [SmallNavbarComponent, ButtonSecondaryComponent],
+  templateUrl: './main-navbar.component.html',
+  styleUrl: './main-navbar.component.scss',
+})
+export class MainNavbarComponent {}
