d0f9d7c617b88d29a47fad9c37153292b6eacf4f
ines0123 <sametines@insat.u-carthage.tn>
2024-12-22 00:28:25 +0100

    fix: update import path for RuleContainerComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {RuleContainerComponent} from "./shared/rule-container/rule-container.component";
+import {RuleContainerComponent} from "./shared/components/rule-container/rule-container.component";
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {LanguageService} from "./shared/services/language.service";
 @Component({
