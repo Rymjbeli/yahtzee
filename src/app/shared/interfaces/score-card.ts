c94a93f24ee31f564599aea652dfaab5b156f284
saradrine <sarra.drine@insat.ucar.tn>
2024-12-22 23:11:57 +0100

    Refactor score card structure to use ScoreCardItem interface and update player initialization; enhance scorecard component styles and HTML for better readability and functionality

--- src/app/shared/interfaces/score-card.ts
+++ src/app/shared/interfaces/score-card.ts
@@ -1,17 +1,23 @@
+export interface ScoreCardItem {
+  picked: boolean;
+  value: number | null;
+}
+
 export interface ScoreCard {
-  aces: number | null;
-  twos: number | null;
-  threes: number | null;
-  fours: number | null;
-  fives: number | null;
-  sixes: number | null;
-  threeOfAKind: number | null;
-  fourOfAKind: number | null;
-  fullHouse: number | null;
-  smallStraight: number | null;
-  largeStraight: number | null;
-  chance: number | null;
-  yahtzee: number | null;
-  bonus: number | null;
-  total: number | null;
+  aces: ScoreCardItem;
+  twos: ScoreCardItem;
+  threes: ScoreCardItem;
+  fours: ScoreCardItem;
+  fives: ScoreCardItem;
+  sixes: ScoreCardItem;
+  threeOfAKind: ScoreCardItem;
+  fourOfAKind: ScoreCardItem;
+  fullHouse: ScoreCardItem;
+  smallStraight: ScoreCardItem;
+  largeStraight: ScoreCardItem;
+  chance: ScoreCardItem;
+  yahtzee: ScoreCardItem;
+  bonus: null;
+  total: null;
+  [key: string]: ScoreCardItem | null;
 }
